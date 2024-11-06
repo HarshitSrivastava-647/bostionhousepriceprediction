ca001c933e7227d249ea554dc839011ec01d6510
HarshitSrivastava-647 <harshit989srivastava@gmail.com>
2024-11-06 14:24:19 +0530

    Released Webapp V1.0

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ scalr=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
+""" 
+Testing API
 
 @app.route('/predic_api',methods=['POST'])
 def predic_api():
@@ -19,6 +21,17 @@ def predic_api():
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+"""
+
+@app.route('/predict',methods=['POST'])
+def predic():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalr.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    op = regmodel.predict(final_input)[0]
+
+   # return redirect(url_for('home', pred_text=pred_text))
+    return render_template('home.html',prediction_text='Predicted House price: {}'.format(op), show_modal=True)
 
 if __name__=="__main__":
     app.run(debug=True)
